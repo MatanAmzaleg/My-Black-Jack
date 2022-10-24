1c9ee69cf7dc73c6845d15ab84dd8732ea5ee8dd
Matan Amzaleg <91034431+UnderDoggy7@users.noreply.github.com>
2022-10-24 21:10:59 +0300

    Add files via upload

--- js/blackjack-controller.js
+++ js/blackjack-controller.js
@@ -11,7 +11,6 @@ function onInit() {
 }
 
 function renderCards() {
-    console.log(getAllPlayersCards());
     var playerCards = getAllPlayersCards()[1].cards
     var dealerCards = getAllPlayersCards()[0].cards
     var playerStrHTML = `
@@ -28,6 +27,25 @@ function renderCards() {
     document.querySelector('.dealer-cards-container').innerHTML = dealerStrHTML
     document.querySelector('.dealer-count-span').innerText = getAmountFromStr(getAllPlayersCards()[0].cards[0])
 
+    checkBlackJack()
+    setTimeout(checkSplit, 500)
+}
+
+function renderSplitCards(){
+    var playerCards = getAllPlayersCards()[1].cards
+    var playerSplitCards = getAllPlayersCards()[2].cards
+    var playerStrHTML = `
+    <img class="card" src="img/cards/${playerCards[0]}.png" alt="">
+    `
+    document.querySelector('.player-cards-container').innerHTML = playerStrHTML
+    document.querySelector('.player-count-span').innerText = getAllPlayersCards()[1].count
+
+    var playerSplitStrHTML = `
+<img class="card" src="img/cards/${playerSplitCards[0]}.png" alt="">
+`
+    document.querySelector('.player-split-cards-container').innerHTML = playerSplitStrHTML
+    document.querySelector('.player-count-split-span').innerText = getAllPlayersCards()[2].count
+
 }
 
 function onHitBtn() {
@@ -37,13 +55,33 @@ function onHitBtn() {
     `
     document.querySelector('.player-cards-container').innerHTML += playerStrHTML
     document.querySelector('.player-count-span').innerText = getAllPlayersCards()[1].count
+    countPlayerAces()
+    countPlayerAceCorrectly()
+    renderScores()
     if (checkIfOver21()) {
-        console.log(document.querySelector("#game-over-modal"));
         $("#game-over-modal").modal('show');
     }
 }
 
+function onSplitHitBtn() {
+    var nextCard = splitHitButton()
+    var playerStrHTML = `
+    <img class="card" src="img/cards/${nextCard}.png" alt="">
+    `
+    document.querySelector('.player-split-cards-container').innerHTML += playerStrHTML
+    document.querySelector('.player-count-split-span').innerText = getAllPlayersCards()[2].count
+    countPlayerSplitAces()
+    countPlayerSplitAceCorrectly()
+    renderScores()
+    if (checkIfSplitOver21()) {
+        $("#game-over-modal").modal('show');
+        document.querySelector('.hand').innerHTML = ''
+    }
+
+}
+
 function onAnotherGame() {
+    resetAcesCount()
     restartGame()
     $("#game-over-modal").modal('hide');
     renderCards()
@@ -51,14 +89,15 @@ function onAnotherGame() {
 
 function onStayBtn() {
     var dealerFlippedCard = getAllPlayersCards()[0].cards[1]
-    console.log(dealerFlippedCard);
-    console.log(document.querySelector('.backed').src );
     document.querySelector('.backed').src = `img/cards/${dealerFlippedCard}.png`
     gDealerHitInterval = setInterval(onDealerTurn, 1000)
 
 }
 
 function onDealerTurn() {
+    countDealerAces()
+    countDealerAceCorrectly()
+    renderScores()
     if (!checkIfDealerIsOver17()) {
         var nextCard = dealerTurn()
         var dealerStrHTML = `
@@ -70,7 +109,57 @@ function onDealerTurn() {
         clearInterval(gDealerHitInterval)
         checkVictoriousPlayer()
     }
+}
 
+function youWon() {
+    document.querySelector('.modal-title').innerText = 'You Won!🏆'
+    document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
+    $("#game-over-modal").modal('show');
+}
 
+function itsATie() {
+    document.querySelector('.modal-title').innerText = 'Its a Tie😁'
+    document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
+    $("#game-over-modal").modal('show');
+}
 
+function youLost() {
+    document.querySelector('.modal-title').innerText = 'You lost☹'
+    document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
+    $("#game-over-modal").modal('show');
+}
+
+function renderBlackJack() {
+    document.querySelector('.modal-title').innerText = 'BlackJack!!♦♥'
+    document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' exactlly'
+    $("#game-over-modal").modal('show');
+}
+
+function renderScores() {
+    document.querySelector('.player-count-span').innerText = getAllPlayersCards()[1].count
+    document.querySelector('.dealer-count-span').innerText = getAllPlayersCards()[0].count
+
+    if (checkIfSplitMode()) {
+        console.log('splitscore');
+        console.log(getAllPlayersCards()[2].count);
+        document.querySelector('.player-count-split-span').innerText = getAllPlayersCards()[2].count
+    }
+}
+
+function renderSplit() {
+    var strHTML =
+        `
+    <div class="hand">
+    <h2>Extra hand: [<span class="player-count-split-span"></span>]</h2>
+    <div class="player-split-cards-container">
+
+    </div>
+    <div class="buttons-div-row">
+        <button onclick="onSplitHitBtn()" type="button" class="btn btn-dark">Hit!</button>
+        <button onclick="onSplitStayBtn()" type="button" class="btn btn-dark">Stay</button>
+    </div>
+</div>
+    `
+
+    document.querySelector('.player-container').innerHTML += strHTML
 }
--- js/services/blackjack-service.js
+++ js/services/blackjack-service.js
@@ -1,18 +1,34 @@
 'use strict'
 
 
+var gIsSplitMode = false
+var gDealerAcesCounted = 0
+var gPlayerAcesCounted = 0
+var gPlayerSplitAcesCounted = 0
 var gDeck = []
 var gDealerCards = {
     cards: [],
-    count: 0
+    count: 0,
+    acesCount: 0
 }
 var gPlayerCards = {
     cards: [],
-    count: 0
+    count: 0,
+    acesCount: 0
 }
 
+var gPlayerSplitCards = {
+    cards: [],
+    count: 0,
+    acesCount: 0
+}
 
-
+function resetAcesCount() {
+    gIsSplitMode = false
+    gPlayerSplitAcesCounted = 0
+    gDealerAcesCounted = 0
+    gPlayerAcesCounted = 0
+}
 
 function createDeck() {
     const nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K']
@@ -27,7 +43,6 @@ function createDeck() {
     for (var i = 0; i < 3; i++) {
         gDeck += deck.concat(deck)
     }
-    console.log(deck.concat(deck));
     gDeck = gDeck.split(",")
     return gDeck
 }
@@ -40,8 +55,6 @@ function shuffleDeck() {
         [gDeck[currentIndex], gDeck[randomIndex]] = [
             gDeck[randomIndex], gDeck[currentIndex]];
     }
-
-    console.log(gDeck);
     return gDeck;
 }
 
@@ -54,8 +67,6 @@ function firstDivision() {
         gPlayerCards.cards.push(secondCard)
         gPlayerCards.count += getAmountFromStr(secondCard)
     }
-    console.log('gDealerCards', gDealerCards.cards, 'gPlayerCards', gPlayerCards.cards, 'gDeck', gDeck);
-    console.log(gDealerCards.count, gPlayerCards.count);
 }
 
 function getAmountFromStr(str) {
@@ -67,19 +78,29 @@ function getAmountFromStr(str) {
 }
 
 function getAllPlayersCards() {
-    return [gDealerCards, gPlayerCards]
+    return [gDealerCards, gPlayerCards, gPlayerSplitCards]
 }
 
 function hitButton() {
     var nextCard = gDeck.pop()
     gPlayerCards.cards.push(nextCard)
     gPlayerCards.count += getAmountFromStr(nextCard)
-    console.log(gPlayerCards, nextCard);
+    return nextCard
+}
+
+function splitHitButton() {
+    var nextCard = gDeck.pop()
+    gPlayerSplitCards.cards.push(nextCard)
+    gPlayerSplitCards.count += getAmountFromStr(nextCard)
     return nextCard
 }
 
 function checkIfOver21() {
     return (gPlayerCards.count > 21)
+}
+
+function checkIfSplitOver21() {
+    return (gPlayerSplitCards.count > 21)
 }
 
 function restartGame() {
@@ -94,28 +115,122 @@ function checkIfDealerIsOver17() {
     return (gDealerCards.count >= 17)
 }
 
-function dealerTurn() { 
-        var nextCard = gDeck.pop()
-        gDealerCards.cards.push(nextCard)
-        gDealerCards.count += getAmountFromStr(nextCard)
-        return nextCard
+function dealerTurn() {
+    var nextCard = gDeck.pop()
+    gDealerCards.cards.push(nextCard)
+    gDealerCards.count += getAmountFromStr(nextCard)
+    return nextCard
 
 }
 
-function checkVictoriousPlayer(){
-    if(gDealerCards.count > 21 || (gPlayerCards.count > gDealerCards.count && gPlayerCards.count < 22)){
-        document.querySelector('.modal-title').innerText = 'You Won!🏆'
-        document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
-        $("#game-over-modal").modal('show');
+function checkVictoriousPlayer() {
+    if (gDealerCards.count > 21 || (gPlayerCards.count > gDealerCards.count && gPlayerCards.count < 22)) youWon()
+    else if (gPlayerCards.count === gDealerCards.count) itsATie()
+    else if (gPlayerCards.count < gDealerCards.count) youLost()
+}
+
+function countDealerAceCorrectly() {
+    if (gDealerCards.count > 21) {
+        for (var i = 0; i < gDealerCards.cards.length; i++) {
+            if (gDealerCards.cards[i].charAt(0) === 'A') {
+                if (gDealerCards.acesCount > gDealerAcesCounted) {
+                    gDealerAcesCounted++
+                    gDealerCards.count -= 10
+                }
+
+            }
+        }
     }
-    else if(gPlayerCards.count === gDealerCards.count){
-        document.querySelector('.modal-title').innerText = 'Its a Tie😁'
-        document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
-        $("#game-over-modal").modal('show');
+}
+
+function countPlayerAceCorrectly() {
+    if (gPlayerCards.count > 21) {
+        for (var i = 0; i < gPlayerCards.cards.length; i++) {
+            if (gPlayerCards.cards[i].charAt(0) === 'A') {
+                if (gPlayerCards.acesCount > gPlayerAcesCounted) {
+                    gPlayerAcesCounted++
+                    gPlayerCards.count -= 10
+                }
+
+            }
+        }
     }
-    else if(gPlayerCards.count < gDealerCards.count){
-        document.querySelector('.modal-title').innerText = 'You lose☹'
-        document.querySelector('.modal-description').innerHTML = 'Youre score is ' + gPlayerCards.count + ' dealer score is ' + gDealerCards.count
-        $("#game-over-modal").modal('show');
+
+}
+
+function countPlayerSplitAceCorrectly() {
+    if (gPlayerSplitCards.count > 21) {
+        for (var i = 0; i < gPlayerSplitCards.cards.length; i++) {
+            if (gPlayerSplitCards.cards[i].charAt(0) === 'A') {
+                if (gPlayerSplitCards.acesCount > gPlayerSplitAcesCounted) {
+                    gPlayerSplitAcesCounted++
+                    gPlayerSplitCards.count -= 10
+                }
+
+            }
+        }
     }
+
+}
+
+
+function countDealerAces() {
+    console.log('counting');
+    var aceCount = 0;
+    for (var i = 0; i < gDealerCards.cards.length; i++) {
+        if (gDealerCards.cards[i].charAt(0) === 'A') {
+            aceCount++
+        }
+    }
+    gDealerCards.acesCount = aceCount
+    console.log('gDealerCards.acesCount', gDealerCards.acesCount);
+}
+
+
+function countPlayerAces() {
+    var aceCount = 0;
+    for (var i = 0; i < gPlayerCards.cards.length; i++) {
+        if (gPlayerCards.cards[i].charAt(0) === 'A') {
+            console.log(gPlayerCards.cards[i].charAt(0));
+            aceCount++
+        }
+    }
+    gPlayerCards.acesCount = aceCount
+    console.log('gPlayerCards.acesCount', gPlayerCards.acesCount)
+}
+
+function countPlayerSplitAces() {
+    var aceCount = 0
+    for (var i = 0; i < gPlayerSplitCards.cards.length; i++) {
+        if (gPlayerSplitCards.cards[i].charAt(0) === 'A') {
+            aceCount++
+        }
+    }
+    gPlayerSplitCards.acesCount = aceCount
+}
+
+function checkBlackJack() {
+    if (gPlayerCards.count === 21 && gDealerCards !== 21) renderBlackJack()
+}
+
+function checkSplit() {
+    if (gPlayerCards.cards[0].charAt(0) === gPlayerCards.cards[1].charAt(0)) {
+        alert('split')
+        if(confirm('do you want to split?')){
+                gIsSplitMode = true
+        gPlayerSplitCards.cards.push(gPlayerCards.cards[0])
+        var poppedCard = gPlayerCards.cards.pop()
+        var amount = getAmountFromStr(poppedCard)
+        gPlayerCards.count -= amount
+        gPlayerSplitCards.count = getAmountFromStr(gPlayerSplitCards.cards[0])
+        renderSplit()
+        renderSplitCards()
+        renderScores()
+        } return
+    
+    }
+}
+
+function checkIfSplitMode() {
+    return gIsSplitMode
 }
